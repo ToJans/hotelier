f64b8fb495a96f4cafff31517a06d5204e1c9051
Rob Ashton <robashton@codeofrob.com>
2013-03-09 21:37:33 +0000

    Skeleton with no test

--- Hotelier/Controllers/HomeController.cs
+++ Hotelier/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using Raven.Client;
 namespace Hotelier.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            throw new NotImplementedException();
+            return View();
         }
     }
 }
---
+++ Hotelier/ViewModels/HotelCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotelier.ViewModels {
+    public class HotelCreateViewModel {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
