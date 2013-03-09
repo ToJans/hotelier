235d96b0314c22ca81a9000ad628a2a7678c13e4
Rob Ashton <robashton@codeofrob.com>
2013-03-09 21:17:21 +0000

    Basic files added for hotel/customer interaction

--- Hotelier/Controllers/HomeController.cs
+++ Hotelier/Controllers/HomeController.cs
@@ -5,12 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using Raven.Client;
 
-namespace Hotelier.Controllers
-{
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
+namespace Hotelier.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Hotelier/Models/Hotel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotelier.Models {
+    public class Hotel {
+        public string Id { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
